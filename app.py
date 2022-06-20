ed16d86e6e65d91f39fc28d51749a92bfa4d1141
JJSEONG <hans3238@naver.com>
2022-06-20 20:12:59 +0900

    Initial commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, render_template, request
+import requests
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def main():
+    myname = "Sparta"
+    return render_template("index.html", name=myname)
+
+
+@app.route('/detail/<keyword>')
+def detail(keyword):
+    r = requests.get('http://openapi.seoul.go.kr:8088/6d4d776b466c656533356a4b4b5872/json/RealtimeCityAir/1/99')
+    response = r.json()
+    rows = response['RealtimeCityAir']['row']
+    word_recevie = request.args.get("word_give")
+    print(word_recevie)
+    return render_template("detail.html", rows=rows, word=keyword)
+
+
+if __name__ == '__main__':
+    app.run('0.0.0.0', port=5000, debug=True)
